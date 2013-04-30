9934c129c6335aa78defc6878561845e2f3bb7fa
Quicksaver <quicksaver@gmail.com>
2013-04-30 13:22:39 +0100

    add TPP context menu entry

--- resource/defaults.js
+++ resource/defaults.js
@@ -3,10 +3,29 @@ var objName = 'thepuzzlepiece';
 var objPathString = 'thepuzzlepiece';
 var prefList = {};
 
+function startAddon(window) {
+	prepareObject(window);
+	window[objName].moduleAid.load(objName, true);
+}
+
+function stopAddon(window) {
+	removeObject(window);
+}
+
 function startConditions(aReason) {
 	return true;
 }
 
-function onStartup(aReason) {}
+function onStartup(aReason) {
+	// Apply the add-on to every window opened and to be opened
+	windowMediator.callOnAll(startAddon, 'navigator:browser');
+	windowMediator.register(startAddon, 'domwindowopened', 'navigator:browser');
+}
 
-function onShutdown(aReason) {}
+function onShutdown(aReason) {
+	// Placing these here prevents an error which I couldn't figure out why the closeCustomize() in overlayAid weren't already preventing.
+	closeCustomize();
+	
+	// remove the add-on from all windows
+	windowMediator.callOnAll(stopAddon, null, null, true);
+}
